34f8f8c56d998bf3f8200cc6ca2f8f7bffc0e1ed
Catherine_Vuthi <vuthi.esther@gmail.com>
2022-04-14 19:36:35 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    let form = document.querySelector("form").addEventListener("submit", (e) => {
+        e.preventDefault();
+        makeTodoList(document.getElementById("new-task-description").value);
+    })
 });
+
+function makeTodoList(todo) {
+    let li = document.createElement("li");
+    let btn = document.createElement("button");
+    btn.addEventListener("click", handleDelete);
+    li.textContent = `${todo} `;
+    btn.textContent = "X";
+    li.appendChild(btn);
+    console.log(li);
+    document.getElementById("tasks").appendChild(li);
+}
+
+function handleDelete(e) {
+    e.target.parentNode.remove();
+}
